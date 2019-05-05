15df0908a60d81fcb8fbce536af99a3782087cf3
NomanIslam <noman.islam@gmail.com>
2019-05-05 02:38:33 -0700

    Practicing If elif and else blocks for Saylni Gulshan Campus

---
+++ even or odd.py
@@ -0,0 +1,5 @@
+number = int(input('Please enter a number to check whether its Even or ODD: '))
+if number % 2 == 0:
+    print('Number ' + str(number) + ' is Even!')
+else:
+    print('This Number i.e. ' + str(number) + ' is Odd')
---
+++ event.py
@@ -0,0 +1,12 @@
+event_venue = input('Where is event?')
+if event_venue == 'Phoopho Home':
+    print('I am busy Can not go!')
+    print("good morning")
+    print("Bye bye")
+elif event_venue == 'Mamoo Home':
+    print('OK I am coming')
+elif event_venue == 'Chcha Home':
+    print('I am coming I miss Chacha')
+else:
+    print('Surely I will go How can I miss a Party!')
+print('After Else')
